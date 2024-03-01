0accc19fd88a53eb82f0cf423c5375e48fab1d7a
Mushrrom <hello@blahaj.zip>
2024-03-01 21:49:11 +1100

    Add travel reasons chart & conclusion

---
+++ charts/travelReasons.js
@@ -0,0 +1,26 @@
+var travelReasons = echarts.init(document.getElementById('travelReasons'), 'dark');
+
+new ResizeObserver(() => travelReasons.resize()).observe(document.getElementById('travelReasons'));
+
+var travelReasonsOptions = {
+  title: {
+    text: 'Reasons that australians travel to'
+  },
+  tooltip: {
+    data: [7, 8, 11, 12, 4, 0, 1, 1]
+  },
+  series: [
+    {
+      name: 'Reasons for travel',
+      type: 'pie',
+      data: [
+          {value: 30, name: 'Both'},
+          {value: 13, name: 'Sightseeing/Holiday'},
+          {value: 5, name: 'Visiting Family/Friends'},
+          {value: 7, name: 'Neither'}
+      ]
+    }
+  ]
+};
+
+travelReasons.setOption(travelReasonsOptions);
